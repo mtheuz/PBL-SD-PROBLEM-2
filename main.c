6f97482d3c531eebcaf4c68431ffa00f7aaa35b8
Matheus Mota <matheuzwork@gmail.com>
2024-06-17 15:09:35 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,12 +6,13 @@ int main()
 {   
     if (open_gpu_device() == 0)
         return 0;
-    
     set_background_color(7, 7, 7); // COR BRANCA NO BACKGROUND
-    set_background_block(0, 0, 7, 0, 0); // BLOCO 8x8 NA POSIÇÃO 0x0 DA TELA (CANTO SUPERIOR ESQUERDO)
-    set_sprite(1, 100, 100, 0, 1); // CRIA SPRITE NA POSIÇÃO 100x100 DA TELA
-    set_poligono(0, 50, 50, 1, 0, 7, 0, 0); // CRIA QUADRADO NA POSIÇÃO 50x50 
-    
+    set_sprite(1, 200, 50,6 , 1); // CRIA SPRITE NA POSIÇÃO 100x100 DA TELA
+    set_poligono(0, 50, 50, 0, 0, 7, 0, 0); // CRIA QUADRADO NA POSIÇÃO 50x50 
+    int i;
+    for (i = 0; i < 2800; i++) {
+        set_background_block_address(i, 0, 0, 0);
+    }
 
     close__gpu_devide();
 
